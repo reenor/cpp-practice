2c849dd127924a85ffc83e0420f4cb69a3fe5785
reedus <reedus.chung@gmail.com>
2022-04-06 15:49:26 +0700

    delete

--- Data-Structures-and-Algorithms-in-C++-2e-Goodrich/C1.8.cpp
+++ Data-Structures-and-Algorithms-in-C++-2e-Goodrich/C1.8.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-std::vector<double> product(std::vector<double> a, std::vector<double> b){
+std::vector<double> product(const std::vector<double> &a, const std::vector<double> &b){
     std::vector<double> c;
     for(size_t i = 0, j = 0; i < a.size() && j < b.size(); ++i, ++j){
         c.push_back(a.at(i) * b.at(j));
